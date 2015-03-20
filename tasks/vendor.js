b05b0c41467873a832d6b516915cbd7dc3deaff0
Chris Basham <csbasham@iu.edu>
2015-03-20 15:23:23 -0400

    Move classNames from npm to bower

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var gzip = require('gulp-gzip');
 
 gulp.task('vendor', function() {
   return gulp.src([
+      './bower_components/classnames/index.js',
       './bower_components/react/react.min.js',
       './bower_components/react-router/build/global/ReactRouter.min.js',
       './bower_components/reflux/dist/reflux.min.js',
@@ -12,7 +14,9 @@ gulp.task('vendor', function() {
     ])
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest('./build'))
+    .pipe(rename('vendor.min.js'))
     //.pipe(uglify())
+    .pipe(gulp.dest('./build'))
     .pipe(gzip())
     .pipe(gulp.dest('./build'));
 });
