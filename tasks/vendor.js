d1ec689e66686a996a715ae6b678c74eda5f14f7
kmurodov <kmurodov@iu.edu>
2015-04-30 12:33:22 -0400

    Bumped react-router version from 0.13.2 to 0.13.3

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -10,7 +10,7 @@ gulp.task('vendor', function() {
   var src = gulp.src([
       './bower_components/classnames/index.js',
       './bower_components/react/react-with-addons.min.js',
-      './bower_components/react-router/build/global/ReactRouter.min.js',
+      './bower_components/react-router/build/umd/ReactRouter.min.js',
       './bower_components/reflux/dist/reflux.min.js',
       './bower_components/fastclick/lib/fastclick.js'
     ])
