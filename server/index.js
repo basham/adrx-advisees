8653cf7b6067a3ee4bb81fc9a02631354766a35e
Chris Basham <csbasham@iu.edu>
2015-05-14 15:46:30 -0400

    Create restify server, autorestarting on file change

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send({ hello: req.params.name });
+  next();
+}
+
+var server = restify.createServer();
+
+server.pre(restify.pre.userAgentConnection());
+
+// curl -is http://localhost:8080/hello/mark | json
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
