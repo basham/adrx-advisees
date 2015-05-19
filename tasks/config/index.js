59e30a9b51ae2e285d90651174ebfe9c895cd94e
Chris Basham <csbasham@iu.edu>
2015-05-19 13:29:41 -0400

    Configure proxy in `package.json` and update readme

--- tasks/config/index.js
+++ tasks/config/index.js
@@ -1,8 +1,13 @@
 var argv = require('yargs').argv;
+var pjson = require('../../package.json');
 
 exports.isProduction = argv.p || argv.production;
 
-var defaultProxy = 'localhost:8000';
-var defaultRemoteProxy = '156.56.176.66:8080';
-var proxy = argv.proxy === 'remote' ? defaultRemoteProxy : argv.proxy;
-exports.proxy = 'http://' + (proxy || defaultProxy);
+// Prefer supplied proxy name first.
+// Otherwise, assume the proxy value is an address.
+// But if nothing is supplied as an argument, then use the default address.
+var defaultProxyName = 'default';
+var proxyName = argv.proxy || defaultProxyName;
+var address = pjson.proxy[proxyName] || argv.proxy || pjson.proxy[defaultProxyName];
+
+exports.proxy = 'http://' + address;
